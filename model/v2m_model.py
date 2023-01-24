01fcb10f2158d65ebe576731e5da79eb5ec20d19
seokhee516 <seokhee051@gmail.com>
2023-01-24 11:55:05 +0000

    refactor: delete frontend.py

--- model/v2m_model.py
+++ model/v2m_model.py
@@ -2,6 +2,6 @@
 
 
 def convert_to_music(video: bytes) -> bytes:
-    
+
     print("영상 파일을 음악 파일로 변환하는 모델 실행")
     return video  # 음악 파일이라고 가정
--- serving/app/main.py
+++ serving/app/main.py
@@ -1,21 +1,24 @@
 import os
 import sys
+
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-INPUT_DIR = os.path.join(BASE_DIR, 'serving/input')
-OUTPUT_DIR =os.path.join(BASE_DIR, 'serving/output')
+INPUT_DIR = os.path.join(BASE_DIR, "serving/input")
+OUTPUT_DIR = os.path.join(BASE_DIR, "serving/output")
 
-sys.path.append(BASE_DIR)
-from model import v2m_model
+# TODO: 다른 서버로 옮긴 후 모델을 실행하고, 다시 웹 서버로 받는 과정 필요
+# sys.path.append(BASE_DIR)
+# from model import v2m_model
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
+@app.get("/") # 시작
 def main():
     return templates.TemplateResponse("index.html", {"request": {}})
 
@@ -23,17 +26,18 @@ def main():
 @app.post("/")
 async def upload(file: UploadFile):
     video = await file.read()  # 파일 읽기
-    music = v2m_model.convert_to_music(video)  # 음악 파일로 변경
-    file_name = file.filename
-    file_path = os.path.join(INPUT_DIR, file_name)
+    
+    file_name = file.filename # 파일 이름 저장
+    file_path = os.path.join(INPUT_DIR, file_name) # input 경로
+    with open(file_path, "wb") as f: # 다른 서버로 넘겨주기 위해 input 데이터 저장
+        f.write(video)
+    # music = v2m_model.convert_to_music(video)  # 음악 파일로 변경
+    return templates.TemplateResponse("result.html", {"request": {"file_name": file_name}}) 
 
-    with open(file_path, "wb") as f:
-        f.write(music)
-    return templates.TemplateResponse("result.html", {"request": {"file_name": file_name}})
 
-
-@app.get("/result/{file_name}")
+@app.get("/result/{file_name}") # 결과
 def download(file_name: str):
-    # file_name mp4 -> wav 형식으로 읽는 코드 수정 필요
-    file_path = os.path.join(OUTPUT_DIR, file_name)
+    # output에 있는 파일은 model을 통해 변환된 음악 파일
+    # TODO: file_name mp4 -> wav 형식으로 읽는 코드 수정 필요
+    file_path = os.path.join(OUTPUT_DIR, file_name) # output 경로 + 파일 이름 
     return FileResponse(file_path)
